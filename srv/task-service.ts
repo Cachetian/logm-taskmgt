b17c23f54ff4542300dc6512f4c7e99cad87a119
Sky Mu <1989mutian@163.com>
2023-10-16 23:29:15 +0800

    Remove custom handler code

--- srv/task-service.ts
+++
@@ -1,8 +0,0 @@
-import { Service } from "@sap/cds/apis/services";
-import { Tasks, Timers } from "#cds-models/sap/logm/db";
-
-export = (srv: Service) => {
-  srv.on("READ", "Timers", (req) => {
-    return [{ instanceId: "1" }];
-  });
-};
